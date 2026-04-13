651522f44cb2b1836ad633426103cc772e0bcc6a
Mohd Faiz <2k23cs2314049@gmail.com>
2026-04-14 01:23:09 +0530

    debugging for http://localhost:8000/api/user/me error

--- client/src/hooks/useGetCurrentUser.jsx
+++ client/src/hooks/useGetCurrentUser.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
-const serverUrl = process.env.REACT_APP_SERVER_URL;
+import { serverUrl } from '../App';
+import { useDispatch } from 'react-redux';
+import { setUserData } from '../redux/userSlice';
 
 
 function useGetCurrentUser() {
+    const dispatch = useDispatch();
     useEffect(() => {
         const getCurrentUser = async () => {
             try{
                 const result = await axios.get(`${serverUrl}/api/user/me`, {withCredentials: true});
-                console.log("Current user:", result.data.user);
+                dispatch(setUserData(result.data))
             }
             catch(error){
                 console.error("Error fetching current user:", error);
